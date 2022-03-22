665d89b0ade4b21352b7759d16fd8afe87d04a9f
Or Gottman <or.gottman@ceva-dsp.com>
2022-03-22 23:32:58 +0200

    Datasets

--- Naive.py
+++
@@ -1,9 +0,0 @@
-from AssociationRule import AssociationRule
-
-
-class Naive:
-    def __init__(self):
-        self.generated_association_rules = list()
-
-    def run(self):
-        a = AssociationRule("a", "b")
